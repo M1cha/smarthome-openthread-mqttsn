0476fb99e33da0dee0b4178a33e330435dc1ef4a
Michael Zimmermann <sigmaepsilon92@gmail.com>
2026-07-04 21:27:00 +0200

    apps: coprocessor: add support for nrf52840dk
    There are random deadlocks and I want to be able to debug them.
    I overwrote the serial number, so I can switch between the dongle and
    the dk without changing the configuration.

--- apps/coprocessor/src/main.c
+++ apps/coprocessor/src/main.c
@@ -11,6 +11,12 @@ LOG_MODULE_REGISTER(ot_br, LOG_LEVEL_DBG);
 
 #define APP_BANNER "***** OpenThread RCP on Zephyr %s *****"
 
+uint8_t *usb_update_sn_string_descriptor(void)
+{
+	static char sn[] = "3804D0A373C4BD19";
+	return sn;
+}
+
 int main(void)
 {
 	LOG_INF(APP_BANNER, CONFIG_NET_SAMPLE_APPLICATION_VERSION);
