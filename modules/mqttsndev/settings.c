d90d52b31fbb574604b8a2ff00f41fe7ee6c298c
Michael Zimmermann <sigmaepsilon92@gmail.com>
2026-07-08 19:42:16 +0200

    mqttsndev: remove most shell commands and add our own
    The idea is to prevent being able to read out the network key via the
    openthread cli. Additionally, we can save a lot of RAM by not enabling
    any shell command, which we don't need.

--- modules/mqttsndev/settings.c
+++ modules/mqttsndev/settings.c
@@ -5,9 +5,15 @@
 
 #ifdef CONFIG_SHELL
 #include <zephyr/shell/shell.h>
+#include <zephyr/sys/util.h>
+#include <openthread.h>
+#include <openthread/thread.h>
 #endif
 
 #include "private.h"
+
+#include <zephyr/logging/log.h>
+LOG_MODULE_DECLARE(mqttsndev, CONFIG_SMARTMETER_MQTTSN_DEVICE_LOG_LEVEL);
 
 struct in6_addr mqttsndev_gateway_ip;
 uint16_t mqttsndev_gateway_port;
@@ -166,11 +172,152 @@ static int cmd_client_id(const struct shell *const sh, const size_t argc, char *
 	return 0;
 }
 
+static int cmd_thread_factoryreset(const struct shell *const sh, const size_t argc, char **argv)
+{
+	struct otInstance *instance = openthread_get_default_instance();
+
+	ARG_UNUSED(argc);
+	ARG_UNUSED(argv);
+
+	openthread_mutex_lock();
+	otInstanceFactoryReset(instance);
+	openthread_mutex_unlock();
+
+	return 0;
+}
+
+static int cmd_thread_start(const struct shell *const sh, const size_t argc, char **argv)
+{
+	otError error;
+	struct otInstance *instance = openthread_get_default_instance();
+
+	ARG_UNUSED(argc);
+	ARG_UNUSED(argv);
+
+	openthread_mutex_lock();
+	error = otThreadSetEnabled(instance, true);
+	if (error != OT_ERROR_NONE) {
+		shell_error(sh, "Failed to start the OpenThread network [%d]", error);
+	}
+	openthread_mutex_unlock();
+
+	return error ? -1 : 0;
+}
+
+static int cmd_thread_stop(const struct shell *const sh, const size_t argc, char **argv)
+{
+	otError error;
+	struct otInstance *instance = openthread_get_default_instance();
+
+	ARG_UNUSED(argc);
+	ARG_UNUSED(argv);
+
+	openthread_mutex_lock();
+	error = otThreadSetEnabled(instance, false);
+	if (error != OT_ERROR_NONE) {
+		shell_error(sh, "Failed to stop the OpenThread network [%d]", error);
+	}
+	openthread_mutex_unlock();
+
+	return error ? -1 : 0;
+}
+
+static int cmd_thread_channel(const struct shell *const sh, const size_t argc, char **argv)
+{
+	otError error;
+	struct otInstance *instance = openthread_get_default_instance();
+	int channel = atoi(argv[1]);
+
+	openthread_mutex_lock();
+	error = otLinkSetChannel(instance, channel);
+	if (error != OT_ERROR_NONE) {
+		shell_error(sh, "Failed to set channel to %d: %d", channel, error);
+	}
+	openthread_mutex_unlock();
+
+	return error ? -1 : 0;
+}
+
+static int cmd_thread_panid(const struct shell *const sh, const size_t argc, char **argv)
+{
+	otError error;
+	struct otInstance *instance = openthread_get_default_instance();
+	const char *panid_str = argv[1];
+	char *endptr = NULL;
+
+	unsigned long panid = strtoul(panid_str, &endptr, 0);
+	if (panid == ULONG_MAX || endptr[0] != 0) {
+		shell_error(sh, "Failed to parse panid '%s'", panid_str);
+		return -EINVAL;
+	}
+
+	openthread_mutex_lock();
+	error = otLinkSetPanId(instance, panid);
+	if (error != OT_ERROR_NONE) {
+		shell_error(sh, "Failed to set pan id to 0x%04lX: %d", panid, error);
+	}
+	openthread_mutex_unlock();
+
+	return error ? -1 : 0;
+}
+
+static int cmd_thread_networkkey(const struct shell *const sh, const size_t argc, char **argv)
+{
+	otError error;
+	struct otInstance *instance = openthread_get_default_instance();
+	const char *network_key_str = argv[1];
+	otNetworkKey network_key = {0};
+	int ret;
+
+	ret = hex2bin(network_key_str, strlen(network_key_str), network_key.m8,
+		      sizeof(network_key.m8));
+	if (ret != sizeof(network_key.m8)) {
+		shell_error(sh, "Failed to parse network key");
+		return -EINVAL;
+	}
+	LOG_HEXDUMP_INF(network_key.m8, sizeof(network_key.m8), "parsed");
+
+	openthread_mutex_lock();
+	error = otThreadSetNetworkKey(instance, &network_key);
+	if (error != OT_ERROR_NONE) {
+		shell_error(sh, "Failed to set network key: %d", error);
+	}
+	openthread_mutex_unlock();
+
+	return error ? -1 : 0;
+}
+
+static int cmd_thread_pollperiod(const struct shell *const sh, const size_t argc, char **argv)
+{
+	otError error;
+	struct otInstance *instance = openthread_get_default_instance();
+	int pollperiod = atoi(argv[1]);
+
+	openthread_mutex_lock();
+	error = otLinkSetPollPeriod(instance, pollperiod);
+	if (error != OT_ERROR_NONE) {
+		shell_error(sh, "Failed to set pollperiod to %d: %d", pollperiod, error);
+	}
+	openthread_mutex_unlock();
+
+	return error ? -1 : 0;
+}
+
 SHELL_STATIC_SUBCMD_SET_CREATE(
 	sub_mqttsndev,
 	SHELL_CMD_ARG(gateway_ip, NULL, "Get/set gateway IP address.", cmd_gateway_ip, 0, 1),
 	SHELL_CMD_ARG(gateway_port, NULL, "Get/set gateway port.", cmd_gateway_port, 0, 1),
 	SHELL_CMD_ARG(client_id, NULL, "Get/set client ID.", cmd_client_id, 0, 1),
+	SHELL_CMD_ARG(thread_factoryreset, NULL, "Delete all thread data", cmd_thread_factoryreset,
+		      0, 0),
+	SHELL_CMD_ARG(thread_start, NULL, "Start thread", cmd_thread_start, 0, 0),
+	SHELL_CMD_ARG(thread_stop, NULL, "Stop thread", cmd_thread_stop, 0, 0),
+	SHELL_CMD_ARG(thread_channel, NULL, "Set thread channel", cmd_thread_channel, 2, 0),
+	SHELL_CMD_ARG(thread_panid, NULL, "Set thread panid", cmd_thread_panid, 2, 0),
+	SHELL_CMD_ARG(thread_networkkey, NULL, "Set thread networkkey", cmd_thread_networkkey, 2,
+		      0),
+	SHELL_CMD_ARG(thread_pollperiod, NULL, "Set thread pollperiod", cmd_thread_pollperiod, 2,
+		      0),
 	SHELL_SUBCMD_SET_END);
 
 SHELL_CMD_REGISTER(mqttsndev, &sub_mqttsndev, "MQTTSN device commands", NULL);
